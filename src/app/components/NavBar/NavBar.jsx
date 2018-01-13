3ff0b643498b37ea46c21d44c39a385acebd35b2
owenthomasanderson@gmail.com <owenthomasanderson@gmail.com>
2018-01-13 13:09:27 -0500

    Added some structure and cleaned up build files

---
+++ src/app/components/NavBar/NavBar.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import * as style from './NavBar.scss';
+
+/**
+ * The component that represents an image in a gallery
+ */
+export class NavBar extends React.Component {
+
+    /**
+     * The default constructor for the Image component
+     */
+    constructor(){
+        super();
+    }
+
+    /**
+     * The render method for the Image component
+     * 
+     * @returns {JSX.Element} - The rendered content for the Image component
+     */
+    render(){
+        return(
+            <nav className={style.className}>
+            
+            </nav>
+        );
+    }
+}
--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { Image } from './components/Image/Image.jsx';
+import { HomePage } from './components/HomePage/HomePage.jsx';
+import { NavBar } from './components/NavBar/NavBar.jsx';
 
 import './index.scss';
 
 
 // <Gallery thumbAspectRatio={1} imageSwitchCoolDownTime={1000} dir='./images/image*.png'/>
 ReactDOM.render(
-    <div id="container"><Image/></div>, document.getElementById('root'));
+    <div id="container"><NavBar/><HomePage/></div>, document.getElementById('root'));
 
