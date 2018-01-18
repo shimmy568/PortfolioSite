027ddcf4555e7d383d6a88651cd2c912d3f702be
owenthomasanderson@gmail.com <owenthomasanderson@gmail.com>
2018-01-17 19:08:12 -0500

    stuf

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -9,5 +9,9 @@ import './index.scss';
 
 // <Gallery thumbAspectRatio={1} imageSwitchCoolDownTime={1000} dir='./images/image*.png'/>
 ReactDOM.render(
-    <div id="container"><NavBar/><HomePage/></div>, document.getElementById('root'));
+    <div id="container">
+    <NavBar/>
+    <HomePage/>
+    </div>,
+    document.getElementById('root'));
 
